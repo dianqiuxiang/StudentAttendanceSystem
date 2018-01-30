ee771af100daa0ba8bb695f7782364755ed92b4e
suiyuan910 <490294043@qq.com>
2018-01-30 14:12:55 +0800

    2018.01.30 14:12

--- cmis/src/com/stuatdsys/cmis/action/BaseAction.java
+++ cmis/src/com/stuatdsys/cmis/action/BaseAction.java
@@ -3,10 +3,5 @@ package com.stuatdsys.cmis.action;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class BaseAction extends ActionSupport  {
-	
-	public BaseAction()
-	{
-		
-	}
 
 }
--- cmis/src/com/stuatdsys/cmis/action/loginAction/LoginAction.java
+++ cmis/src/com/stuatdsys/cmis/action/loginAction/LoginAction.java
@@ -7,15 +7,20 @@ import javax.annotation.Resource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
 import com.opensymphony.xwork2.ActionSupport;
 import com.stuatdsys.cmis.action.BaseAction;
 import com.stuatdsys.cmis.model.Tbl_user;
 import com.stuatdsys.cmis.service.UserService;
 
-@Controller("LoginAction") 
-@Scope("prototype")
-public class LoginAction extends BaseAction {
+import net.sf.json.groovy.GJson;
+
+@Controller
+@RequestMapping(value="/login")
+public class LoginAction{
 	
 	private List<Tbl_user> userList;
 
@@ -28,23 +33,28 @@ public class LoginAction extends BaseAction {
 	}
 	@Resource
 	private UserService userService;
+	
+	public UserService getUserService() {
+		return userService;
+	}
 
-	public String select() {
-		
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@RequestMapping(value="/select.do")
+	@ResponseBody
+	public Object select() {
+		Gson gson = new Gson();
 		System.out.println(123);
 		//调用存储过程，获取列表
 		//this.userService.get(1);
 		System.out.println(this.userService.selectPK(91).getUsername());
-		return SUCCESS;
-	}
-	
-	public String login()
-	{
-		return null;
+		return gson.toJson("123");
 	}
 	 public String execute() {
 //		 this.contrList = this.tabService.queryContractList(new Contract());
 
-	        return SUCCESS;
+	        return "";
 	    }
 }
